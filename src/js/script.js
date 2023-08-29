d1eb42203da1f6edde095853b5b0fa6fd1e1a688
kamal <kamade123@gmail.com>
2023-08-29 01:43:47 +0100

    ...🚀

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,1 @@
-console.log('script');
-import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
